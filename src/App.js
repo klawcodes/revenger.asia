a9b5dc45b9bc02d12349aadac09fd809e34a17f7
klaw <klaw@secretego.localdomain>
2023-08-08 12:10:20 +0700

    commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import './app.css';
+
+function App() {
+  return (
+    <div className="App">
+        <div className='ppr'>
+          <div className='pxx'>
+            <div className="pr">
+              <p>Riot is a passionate 2D + 3D Artist.  I'm currently lending my creative talents to a wide range of projects, from independent productions to established brands. My expertise spans the realm of visual artistry, encompassing everything from designing mesmerizing visualizers for short music videos to orchestrating intricate lighting compositions and bringing 3D animations to life.</p>
+              <p>I thrive on pushing creative boundaries and bringing imagination to reality.</p>
+            </div>
+            <div className='t'>
+              <div className='link'>
+                <a target="_blank" href="">Contact</a>
+              </div>
+              <div className='link'>
+                <a target="_blank" href="https://instagram.com/riotrevenger">Instagram</a>
+              </div>
+              <div className='link'>
+                <a target="_blank" href="https://x.com/RIOTREVENGER">X</a>
+              </div>
+              <div className='link'>
+                <a target="_blank" href="https://rarible.com/riotrevenger">Rarible</a>
+              </div>
+            </div>
+          </div>
+        </div>
+    </div>
+  );
+}
+
+export default App;
