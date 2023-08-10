db173fbeedbf93ddb01aa71978009158db6cbf64
klaw <klaw@secretego.localdomain>
2023-08-10 16:49:11 +0700

    add buch

--- src/components/project/project.jsx
+++ src/components/project/project.jsx
@@ -2,7 +2,7 @@ import React, {useState, useRef, useEffect} from "react";
 import "./project.css"
 import kereb from '../media/KEREB 2.webm'
 import giv from '../media/rxbxg.webm'
-import skrill from '../media/RIOT x Skrill.mp4'
+import skrill from '../media/RIOT-x-Skrill.webm'
 
 const Project = () => {
     return (
@@ -17,7 +17,7 @@ const Project = () => {
                         *WORLD TRANSMUTATION*
                     </text>
                     <p>3D modeling, animation, lighting, and rendering using SVDDEN DEATH's song "KEREBEROT"</p>
-                    <a href="">Original post on Instagram</a>
+                    <a target="_blank" href="https://www.instagram.com/p/Ct_iDdQyAjd/?img_index=1">Original post on Instagram</a>
                 </div>
             </div>
             <div className="vid">
@@ -29,19 +29,19 @@ const Project = () => {
                         GIVENCHY SS23
                     </text>
                     <p>3D modeling, animation, lighting, and graphic motion design for Givenchy SS23 (fake project)</p>
-                    <a href="">Original post on Instagram</a>
+                    <a target="_blank" href="https://www.instagram.com/p/Cvb8PAWygEV/?img_index=1">Original post on Instagram</a>
                 </div>
             </div>
             <div className="vid">
                 <video data-setup='{}' autoPlay="true" loop="true" muted="true" playsInline>
-                    <source src={skrill} type="video/mp4" />
+                    <source src={skrill} type="video/webm" />
                 </video>
                 <div className="text">
                     <text>
                         GOOD SPACE
                     </text>
                     <p>animation, lighting, and 3d modeling character from Alfred Pietroni using music from Skrillex ft. Starrah - Good Space</p>
-                    <a href="">Original post on Instagram</a>
+                    <a target="_blank" href="https://www.instagram.com/p/Couv2bOpacQ/">Original post on Instagram</a>
                 </div>
             </div>
             
