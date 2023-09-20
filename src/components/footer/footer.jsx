10da6d7d8437e9df7530c0d084a4348278db2031
Klaw <klawcodes2gmail.com>
2023-09-20 19:58:00 +0700

    change footer

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -4,7 +4,7 @@ import './footer.css'
 const Footer = () => {
     return (
         <div className="foot">
-            <p id="proud">"EVERYTHING YOU LIKE, I LIKED FIVE YEARS AGO"</p>
+            <p id="proud">"I LOST IT ALL, I DON'T KNOW HOW IM STILL WINNING"</p>
             <h1 id="footer">© 2023 RIOT REVENGER. All rights reserved.</h1>
         </div>
     )
